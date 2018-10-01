869c06ce7a769e8fad5390df25dde8f0d80d734b
drinkthekoolaid <43705247+drinkthekoolaid@users.noreply.github.com>
2018-10-01 09:55:48 -0400

    Add files via upload

---
+++ Paying_down_debt_in_a_year.py
@@ -0,0 +1,28 @@
+balance = 999999
+annualInterestRate = 0.18
+
+
+monthlyinterest = annualInterestRate / 12
+tempbalance = balance
+epsilon = 0.01
+monthlypay = tempbalance / 12
+lowp = tempbalance / 12
+highp = (tempbalance * (1 + monthlyinterest)**12) / 12.0
+
+while abs(balance) > epsilon:
+    balance = tempbalance
+    monthlypay = (lowp + highp) / 2
+    for i in range(12):   
+        unpaidbalance = balance - monthlypay
+        interest = unpaidbalance * (annualInterestRate / 12)
+        balance = unpaidbalance + interest   
+    if balance > epsilon:
+        lowp = monthlypay
+
+    elif balance < epsilon:
+        highp = monthlypay
+
+    else:
+        break
+        
+print(round(monthlypay, 2))
